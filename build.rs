b83253a23f3eaa55c526deed3a13a0d627a2bf1c
FoxMoss <foxmoss@mediaology.com>
2026-01-07 03:58:30 -0600

    fix: clip -> clipers

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    cxx_build::bridge("src/main.rs")
+    cxx_build::bridge("src/lib.rs")
         .file("clip.cpp")
         .file("rust_interface.cpp")
         .flag("-Wno-ignored-qualifiers")
