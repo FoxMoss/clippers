e1f59877fa9e908ec114929d40b5aa723c2cec4d
FoxMoss <foxmoss@mediaology.com>
2026-01-07 13:55:13 -0600

    revert to e342d62

--- build.rs
+++ build.rs
@@ -5,36 +5,37 @@ fn main() {
         .flag("-Wno-ignored-qualifiers")
         .flag("-Wno-sign-compare")
         .flag("-Wno-unused-variable")
-        .flag("-march=native")  
         .flag("-mavx")
-        .flag("-mavx2")
+        // .flag("-mavx2")
+        // .flag("-mavx512f")_mm256_fmadd_ps
+        // .flag("-mavx512vbmi")
+        // .flag("-mavx512vnni")
         .flag("-mfma")
         .flag("-mf16c")
         .flag("-O3")
-        .flag("-ffast-math")
-        .flag("-funroll-loops")
         .compile("clipcpp");
     
     cc::Build::new()
         .file("ggml/src/ggml.c")
         .flag("-std=c11")
         .flag("-Wno-unused-variable")
-        .flag("-march=native")
         .flag("-mavx")
-        .flag("-mavx2")
+        // .flag("-mavx2")
+        // .flag("-mavx512f")
+        // .flag("-mavx512vbmi")
+        // .flag("-mavx512vnni")
         .flag("-mfma")
         .flag("-mf16c")
         .flag("-O3")
-        .flag("-ffast-math")
-        .flag("-funroll-loops")
-        // .flag("-DGGML_USE_OPENBLAS")
+        .flag("-DGGML_USE_OPENBLAS")
         .define("_GNU_SOURCE", None) 
         .define("_POSIX_C_SOURCE", "200809L")  
         .compile("ggml");
-    
+
     println!("cargo:rerun-if-changed=clip.cpp");
     println!("cargo:rerun-if-changed=rust_interface.cpp");
     println!("cargo:rerun-if-changed=clip.h");
     println!("cargo:rerun-if-changed=ggml/src/ggml.c");
+
     println!("cargo:rustc-link-lib=cblas");
 }
